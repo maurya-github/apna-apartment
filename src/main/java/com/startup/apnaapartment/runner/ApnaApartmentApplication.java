97870c72baaaf6818dbb4cedc55eabbd0ba2b170
amit.mau <amitmau@zeta.tech>
2020-03-27 12:50:16 +0530

    Initial commit

---
+++ src/main/java/com/startup/apnaapartment/runner/ApnaApartmentApplication.java
@@ -0,0 +1,13 @@
+package com.startup.apnaapartment.runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApnaApartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApnaApartmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/startup/apnaapartment/ApnaApartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startup.apnaapartment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApnaApartmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
